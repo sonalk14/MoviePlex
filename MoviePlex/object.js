f3397a2d8f665c561357ed0f960999e079c774aa
sonalk14 <143019073+sonalk14@users.noreply.github.com>
2024-05-16 08:22:45 +0530

    Add files via upload

---
+++ MoviePlex/object.js
@@ -0,0 +1,76 @@
+ let x=[
+    {
+        image: "img1.jpg",
+        name: "spiderman Across the spider-verse",
+        seats: 25,
+        price: 350
+    },
+    {
+        image: "img2.jpg",
+        name: "Elementals",
+        seats: 30,
+        price: 250
+    },
+    {
+        image: "img3.jpg",
+        name: "Transformer-Rise of beasts",
+        seats: 20,
+        price: 200
+    },
+    {
+        image: "img4.jpg",
+        name: "Black Panther",
+        seats: 10,
+        price: 400
+    },
+    {
+        image: "img6.jpg",
+        name: "Spiderman-Far from home",
+        seats: 25,
+        price: 300
+    },
+    {
+        image: "img7.jpg",
+        name: "Bharat",
+        seats: 25,
+        price: 300
+    },
+    {
+        image: "img8.jpg",
+        name: "Captain Marvel",
+        seats: 20,
+        price: 350
+    },
+    {
+        image: "img9.jpg",
+        name: "Doctor Strange-Multiverse of Madness",
+        seats: 15,
+        price: 350
+    },
+    {
+        image: "show1.jpg",
+        name: "Tamas",
+        seats: 42,
+        price: 150
+    },
+    {
+        image: "show2.jpg",
+        name: "Ramkali",
+        seats: 30,
+        price: 200
+    },
+
+
+    {
+        image: "show3.jpg",
+        name: "Adhura",
+        seats: 35,
+        price: 250
+    },
+    {
+        image:"show4.jpg",
+        name:"Dasha",
+        seats:25,
+        price:350
+    }
+]
